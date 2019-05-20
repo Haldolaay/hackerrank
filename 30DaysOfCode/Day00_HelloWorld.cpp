7256c0f6d57a2e20d39642eb16cdc13c712b133f
Hamzah Aldolaay <41927016+Haldolaay@users.noreply.github.com>
2019-05-19 22:43:06 -0700

    adding some comments if that's ok

--- 30DaysOfCode/Day00_HelloWorld.cpp
+++ 30DaysOfCode/Day00_HelloWorld.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-using namespace std;
+#include <iostream>// calling a library into our programe, we can't use cout without it
+using namespace std;// strings need std:: operater, using namespace saves us from writing std everytime
 
 int main() {
     string str;
     getline(cin, str);
-    cout << "Hello, World." << endl;
+    cout << "Hello, World." << endl; // cout is a stream, using the << operater, we are sending the message to be shown on the stream
     cout << str << endl;
 }
